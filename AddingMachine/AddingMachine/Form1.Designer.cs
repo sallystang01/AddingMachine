3012f608be64833c5807060006398de480c877f5
sallystang01 <hunterduerr@gmail.com>
2018-04-25 21:50:35 -0400

    Broken Brain

---
+++ AddingMachine/AddingMachine/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace AddingMachine
+{
+    partial class AddingMachine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.lbNumbers = new System.Windows.Forms.ListBox();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.lblCalc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a number to get started:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(71, 43);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(230, 20);
+            this.tbInput.TabIndex = 1;
+            this.tbInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbInput_KeyDown);
+            // 
+            // lbNumbers
+            // 
+            this.lbNumbers.FormattingEnabled = true;
+            this.lbNumbers.Location = new System.Drawing.Point(71, 84);
+            this.lbNumbers.Name = "lbNumbers";
+            this.lbNumbers.Size = new System.Drawing.Size(230, 173);
+            this.lbNumbers.TabIndex = 2;
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnTotal.FlatAppearance.BorderSize = 0;
+            this.btnTotal.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumSpringGreen;
+            this.btnTotal.FlatAppearance.MouseOverBackColor = System.Drawing.Color.PaleGreen;
+            this.btnTotal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTotal.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTotal.Location = new System.Drawing.Point(122, 328);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(124, 31);
+            this.btnTotal.TabIndex = 3;
+            this.btnTotal.Text = "Get Total!";
+            this.btnTotal.UseVisualStyleBackColor = false;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // lblCalc
+            // 
+            this.lblCalc.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCalc.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalc.Location = new System.Drawing.Point(67, 274);
+            this.lblCalc.Name = "lblCalc";
+            this.lblCalc.Size = new System.Drawing.Size(234, 23);
+            this.lblCalc.TabIndex = 4;
+            this.lblCalc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AddingMachine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 387);
+            this.Controls.Add(this.lblCalc);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.lbNumbers);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Location = new System.Drawing.Point(500, 150);
+            this.MaximizeBox = false;
+            this.Name = "AddingMachine";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Adding Machine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.ListBox lbNumbers;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Label lblCalc;
+    }
+}
+
---
+++ AddingMachine/AddingMachine/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddingMachine
+{
+    public partial class AddingMachine : Form
+    {
+        double[] numbers = new double[10];
+        int i = 0;
+        double sum = 0;
+
+        public AddingMachine()
+        {
+            InitializeComponent();
+        }
+
+        private void tbInput_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                lbNumbers.Items.Add(tbInput.Text);
+                tbInput.Text = "";
+                this.Size = new Size(388, 413);
+                numbers[i] = double.Parse(tbInput.Text);
+                // insert code here dingdong
+
+
+                sum = sum + numbers[i];
+                //lblCalc.Text = sum.ToString();
+                i++;
+            }
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+
+
+
+            lblCalc.Text = numbers.Sum().ToString();
+        }
+
+    }
+}
