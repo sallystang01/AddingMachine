c667aaa75bb282ca3a9dd3b30b0b49145bbdcf16
sallystang01 <hunterduerr@gmail.com>
2018-04-26 19:32:38 -0400

    Final

--- AddingMachine/AddingMachine/Form1.Designer.cs
+++ AddingMachine/AddingMachine/Form1.Designer.cs
@@ -33,16 +33,18 @@
             this.lbNumbers = new System.Windows.Forms.ListBox();
             this.btnTotal = new System.Windows.Forms.Button();
             this.lblCalc = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(68, 17);
+            this.label1.Location = new System.Drawing.Point(68, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(234, 23);
+            this.label1.Size = new System.Drawing.Size(233, 31);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Enter a number to get started:";
+            this.label1.Text = "Type a number then hit Enter to get started:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // tbInput
@@ -50,17 +52,17 @@
             this.tbInput.Location = new System.Drawing.Point(71, 43);
             this.tbInput.Name = "tbInput";
             this.tbInput.Size = new System.Drawing.Size(230, 20);
-            this.tbInput.TabIndex = 1;
+            this.tbInput.TabIndex = 0;
             this.tbInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.tbInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbInput_KeyDown);
             // 
             // lbNumbers
             // 
             this.lbNumbers.FormattingEnabled = true;
-            this.lbNumbers.Location = new System.Drawing.Point(71, 84);
+            this.lbNumbers.Location = new System.Drawing.Point(71, 117);
             this.lbNumbers.Name = "lbNumbers";
             this.lbNumbers.Size = new System.Drawing.Size(230, 173);
-            this.lbNumbers.TabIndex = 2;
+            this.lbNumbers.TabIndex = 0;
             // 
             // btnTotal
             // 
@@ -69,30 +71,65 @@
             this.btnTotal.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumSpringGreen;
             this.btnTotal.FlatAppearance.MouseOverBackColor = System.Drawing.Color.PaleGreen;
             this.btnTotal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnTotal.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTotal.Location = new System.Drawing.Point(122, 328);
+            this.btnTotal.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTotal.Location = new System.Drawing.Point(67, 345);
             this.btnTotal.Name = "btnTotal";
-            this.btnTotal.Size = new System.Drawing.Size(124, 31);
-            this.btnTotal.TabIndex = 3;
+            this.btnTotal.Size = new System.Drawing.Size(234, 31);
+            this.btnTotal.TabIndex = 1;
             this.btnTotal.Text = "Get Total!";
             this.btnTotal.UseVisualStyleBackColor = false;
             this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
             // 
             // lblCalc
             // 
+            this.lblCalc.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.lblCalc.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lblCalc.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCalc.Location = new System.Drawing.Point(67, 274);
+            this.lblCalc.Location = new System.Drawing.Point(67, 307);
             this.lblCalc.Name = "lblCalc";
             this.lblCalc.Size = new System.Drawing.Size(234, 23);
             this.lblCalc.TabIndex = 4;
             this.lblCalc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Gold;
+            this.btnClear.FlatAppearance.BorderSize = 0;
+            this.btnClear.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow;
+            this.btnClear.FlatAppearance.MouseOverBackColor = System.Drawing.Color.PaleGoldenrod;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(67, 382);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(113, 31);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear!";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.IndianRed;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.RosyBrown;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(128, 74);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(113, 31);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit!";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.button2_Click);
+            // 
             // AddingMachine
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(372, 387);
+            this.ClientSize = new System.Drawing.Size(372, 115);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
             this.Controls.Add(this.lblCalc);
             this.Controls.Add(this.btnTotal);
             this.Controls.Add(this.lbNumbers);
@@ -104,6 +141,7 @@
             this.Name = "AddingMachine";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Adding Machine";
+            this.Load += new System.EventHandler(this.AddingMachine_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,6 +154,8 @@
         private System.Windows.Forms.ListBox lbNumbers;
         private System.Windows.Forms.Button btnTotal;
         private System.Windows.Forms.Label lblCalc;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- AddingMachine/AddingMachine/Form1.cs
+++ AddingMachine/AddingMachine/Form1.cs
@@ -12,41 +12,123 @@ namespace AddingMachine
 {
     public partial class AddingMachine : Form
     {
+        // Declares variables that will be used through program
         double[] numbers = new double[10];
         int i = 0;
         double sum = 0;
+      
+        
 
         public AddingMachine()
         {
             InitializeComponent();
         }
-
+        private void AddingMachine_Load(object sender, EventArgs e)
+        {
+            tbInput.Focus();
+        }
         private void tbInput_KeyDown(object sender, KeyEventArgs e)
         {
-
-
-            if (e.KeyCode == Keys.Enter)
+            try
             {
-                lbNumbers.Items.Add(tbInput.Text);
-                tbInput.Text = "";
-                this.Size = new Size(388, 413);
-                numbers[i] = double.Parse(tbInput.Text);
-                // insert code here dingdong
+                // When enter key is struck, add number from text box into the array and add the number
+                // to the list box.
+                if (e.KeyCode == Keys.Enter)
+                { // If number in text box is greater than 0, proceed.
+                    if (Convert.ToDouble(tbInput.Text) > 0)
+                    {
+                        // Adds the number to the listbox
+                        lbNumbers.Items.Add(tbInput.Text);
 
+                        // Resizes form
+                        this.Size = new Size(388, 470);
 
-                sum = sum + numbers[i];
-                //lblCalc.Text = sum.ToString();
-                i++;
+                        // Adds the number to the array
+                        numbers[i] = double.Parse(tbInput.Text);
+
+                        // Clears the text box and prepares it for a new number
+                        tbInput.Text = "";
+                       
+
+                        // Sum variable = sum + new number in array
+                        sum = sum + numbers[i];
+                       
+                        // Increment i to the next place in the array
+                        i++;
+                        // Changes location of exit button
+                        btnExit.Location = new Point(188, 382);
+
+                        label1.Text = "Continue adding numbers...";
+                    }
+
+                    else
+                    {
+                        // Error handeling
+                        MessageBox.Show("Please enter a value greater than 0!");
+                    }
+                }
+
+                // Clears array before it becomes full.
+                if (i > (numbers.Length - 1))
+                {
+                    sum = numbers.Sum();
+                    Array.Clear(numbers, 0, numbers.Length);
+                    i = 0;
+                }
             }
+
+            catch (Exception)
+            {
+                // Error handeling
+                MessageBox.Show("Please enter numeric values only!");
+            }
+
         }
+
+
+
+
+
 
         private void btnTotal_Click(object sender, EventArgs e)
         {
 
+            // Displays the sum to the label
 
-
-            lblCalc.Text = numbers.Sum().ToString();
+            lblCalc.Text = sum.ToString();
+            // Puts the focus back to the text box
+            tbInput.Focus();
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Confirm that user really intends on exiting the application.
+            DialogResult dr = MessageBox.Show("Are you sure you want to exit?", "Exiting...", MessageBoxButtons.YesNo);
+
+            if (dr == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Clears everything and prepares the form for a new set of data
+            Array.Clear(numbers, 0, numbers.Length);
+            lbNumbers.Items.Clear();
+            tbInput.Focus();
+            lblCalc.Text = "";
+            i = 0;
+            sum = 0;
+            this.Size = new Size(388, 153);
+            btnExit.Location = new Point(128, 74);
+            label1.Text = "Type a number then hit Enter to get started:";
+        }
+
+        
     }
+
+   
+        
+       
 }
